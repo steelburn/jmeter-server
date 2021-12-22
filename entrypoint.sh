985b104f956823020ce6eebe7d5e8e0736cead3d
steelburn <steelburn@gmail.com>
2021-12-22 18:43:08 +0800

    Updated entrypoint

--- entrypoint.sh
+++ entrypoint.sh
@@ -19,7 +19,6 @@ n=$(($freeMem/10*2))
 export JVM_ARGS="-Xmn${n}m -Xms${s}m -Xmx${x}m"
 JMETER_LOG="jmeter-server.log" && touch $JMETER_LOG && tail -f $JMETER_LOG &
 exec jmeter-server \
-    -D "java.rmi.server.hostname=${IP}" \
     -D "client.rmi.localport=${RMI_PORT}" \
     -D "server.rmi.localport=${RMI_PORT}" \
     -Jserver.rmi.ssl.disable=true
